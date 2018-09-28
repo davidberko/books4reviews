f1bf168d6e92d2c39f6faa17aa80e50206f7a720
davidberko <davidmichaelberko@gmail.con>
2018-09-28 14:04:39 -0500

    added root

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,16 +3,17 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  prepend_before_action :require_no_authentication, only: :cancel
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  #GET /resource/sign_up
+   #def new
+    # super
+   #end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+   #POST /resource
+   #def create
+    # super
+   #end
 
   # GET /resource/edit
   # def edit
@@ -38,12 +39,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+   def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:access_level])
+   end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
           sessions: 'users/sessions',
           registrations: 'users/registrations'
         }
+
+  root to: 'home_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
