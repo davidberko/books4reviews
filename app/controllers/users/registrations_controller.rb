2daf8a36f225c09495b0d55d00bf646866b4bd92
davidberko <davidmichaelberko@gmail.con>
2018-09-28 15:33:23 -0500

    current changes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+prepend_before_action :require_no_authentication, only: :cancel
   # GET /resource/sign_up
   # def new
   #   super
@@ -59,4 +59,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  private
+
+ def sign_up_params
+   params.require(:user).permit(:access_level, :first_name, :last_name, :email, :password, :password_confirmation)
+ end
+
+ def account_update_params
+   params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+ end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_180854) do
+ActiveRecord::Schema.define(version: 2018_09_28_201856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 2018_09_28_180854) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "access_level"
+    t.string "first_name"
+    t.string "last_name"
+    t.index ["access_level"], name: "index_users_on_access_level"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
