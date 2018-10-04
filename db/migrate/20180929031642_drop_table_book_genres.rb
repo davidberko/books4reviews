bee1dbecd5d5c79b480cb08b6aa722aac8a79310
davidberko <davidmichaelberko@gmail.com>
2018-10-04 16:19:39 -0400

    form styling

--- db/migrate/20180929031642_drop_table_book_genres.rb
+++
@@ -1,5 +0,0 @@
-class DropTableBookGenres < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :book_genres 
-  end
-end
--- db/migrate/20181001182543_add_text_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddTextToReviews < ActiveRecord::Migration[5.2]
-  def change
-    add_column :reviews, :comment, :text
-  end
-end
