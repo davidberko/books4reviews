578af9685affc57e850cafa237cb971ab27a458f
davidberko <davidmichaelberko@gmail.com>
2019-04-09 20:14:55 -0500

    making commit before changing branches

--- db/schema.rb
+++ db/schema.rb
@@ -60,18 +60,6 @@ ActiveRecord::Schema.define(version: 2018_10_11_030437) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
   create_table "notifications", force: :cascade do |t|
     t.integer "recipient_id"
     t.integer "actor_id"
